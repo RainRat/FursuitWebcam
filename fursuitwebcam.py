4bf054090296ad224e51feb1a5259c6fe202bb9f
RainRat <rainrat78@yahoo.ca>
2022-02-13 15:35:33 -0800

    Try counting the number of fursuits as well (experimental)
    Add a neural network to try counting the number of fursuits. Plus update the recognition to latest version.

--- fursuitwebcam.py
+++ fursuitwebcam.py
@@ -11,7 +11,7 @@ for row in csv_reader:
     fursuitnames.append(row)
     
 modelfursuitname = load_model('fursuitname.h5')
-
+modelfursuitcount = load_model('fursuitcount.h5')
 cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
@@ -20,6 +20,7 @@ while True:
     test_image = image.img_to_array(test_image)
     test_image = np.expand_dims(test_image, axis=0)
     test_image=test_image/255.0
+    
     fursuitnameresult = (modelfursuitname.predict(test_image, batch_size=1))[0]
     maxcon=0.
     for idx, val in enumerate(fursuitnameresult):
@@ -28,9 +29,22 @@ while True:
             maxcon=val
 #    print(maxidx)
     thereply=fursuitnames[maxidx][1]+ ": "+'{percent:.0%}'.format(percent=(maxcon))
+                
+    fursuitcountresult = (modelfursuitcount.predict(test_image, batch_size=1))[0]
+    fursuitcountnames=["0", "1", "2", "3+"]
+    maxcon=0.
+    for idx, val in enumerate(fursuitcountresult):
+        if val>maxcon:
+            maxidx=idx
+            maxcon=val
+    thereply2="# of fursuits: "+fursuitcountnames[maxidx]+ " ("+'{percent:.0%}'.format(percent=(maxcon))+")"
+
+
+
     frame=cv2.resize(frame, (1024, 780))
 #    cv2.putText(frame, thereply, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
     cv2.putText(frame, thereply, (20, 75), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 2)
+    cv2.putText(frame, thereply2, (20, 740), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 2)
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
